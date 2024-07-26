46afdf7826058f48d059ebcfc048b2e9909000c0
Immer01 <immermwe@gmail.com>
2024-07-26 09:31:06 +0300

    Add files via upload

---
+++ convertcelcius.cs
@@ -0,0 +1,10 @@
+﻿// Converting to Celsius
+
+int fahrenheit = 94;
+decimal temperature = ((fahrenheit - 32) * 5m / 9);
+Console.WriteLine("The temperature is " + temperature + " degrees Celsius");
+
+fahrenheit = 94;
+temperature = ((fahrenheit - 32m) * 5m / 9m);
+Console.WriteLine("The temperature is " + Math.Round(temperature) + " degrees Celsius");
+
