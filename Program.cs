ebfd3a510b4f3028111a2e7996e988bed59c51b2
Immer01 <immermwe@gmail.com>
2024-07-26 09:05:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// Data Types
+
+Console.WriteLine("Signed integral types:");
+Console.WriteLine($"sbyte : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int : {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long : {long.MinValue} to {long.MaxValue}");
